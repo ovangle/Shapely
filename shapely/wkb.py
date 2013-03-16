fc2859bbf7c668ad680c4f91accfca2ba405bf59
Thomas Stephenson <ovangle@gmail.com>
2013-03-16 22:00:52 +1100

    Restored loads and dumps functions
    With appropriate deprecation warnings added

--- shapely/wkb.py
+++ shapely/wkb.py
@@ -1,11 +1,19 @@
 """Load/dump geometries using the well-known binary (WKB) format
 """
 
+import warnings
+
 from shapely.geometry.base import geom_from_wkb, geom_to_wkb
 
 # Pickle-like convenience functions
 
-##TODO: Restore loads and dumps
+def loads(data):
+    """Load a geometry from a WKB string representing the byte"""
+    warnings.warn(
+        "Deprecated. Use load_bytes instead", 
+        DeprecationWarning)
+    return load_bytes(data.encode('latin1'))
+    
 
 def load_bytes(data):
     """Load a geometry from a WKB bytes object."""
@@ -15,6 +23,13 @@ def load(fp):
     """Load a geometry from an open file."""
     data = fp.read()
     return loads(data)
+
+def dumps(ob):
+    """Dump a WKB representation of a geometry to a string"""
+    warnings.warn(
+        "Deprecated. Use dump_bytes instead", 
+        DeprecationWarning)
+    return dump_bytes(ob).decode('latin1')
 
 def dump_bytes(ob):
     """Dump a WKB representation of a geometry to a byte string."""
